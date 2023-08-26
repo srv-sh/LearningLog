b3f2802f8b5d8fe99361a426d16494409edf69ba
srv-sh <data.geek.srv@gmail.com>
2023-08-26 11:35:55 +0600

    added views of app

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """The Home page for Learning Log"""
+
+    return render(request, 'learning_logs/index.html')
