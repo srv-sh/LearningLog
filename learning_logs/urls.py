80a6bf6237b7bc170efce3ecbab01e5205bb98f1
srv-sh <data.geek.srv@gmail.com>
2023-08-26 15:15:20 +0600

    added urls for topic and topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,7 +4,11 @@
 from django.urls import re_path as url
 from . import views
 
+app_name = 'learning_logs'
+
 urlpatterns = [
     # Home page
-    url(r'^$', views.index, name = 'index')
+    url(r'^$', views.index, name = 'index'),
+    url(r'^topics/$', views.topics, name = 'topics'),
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
 ]
