581844ff6dd72053d8697d4909b28d6e2d5325dd
srv-sh <data.geek.srv@gmail.com>
2023-08-26 21:12:34 +0600

    added all urls code

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,5 +10,11 @@ urlpatterns = [
     # Home page
     url(r'^$', views.index, name = 'index'),
     url(r'^topics/$', views.topics, name = 'topics'),
-    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    #Page for adding a new topic 
+    url(r'^new_topic/$', views.new_topic , name = 'new_topic'),
+    #page for adding a new entry to a topic
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry , name = 'new_entry'),
+    # page for editing an entry
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name = 'edit_entry'),
 ]
